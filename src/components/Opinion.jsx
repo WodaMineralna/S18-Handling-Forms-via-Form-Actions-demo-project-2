c511519a3cc320d5e871098640d9229783502ae7
WodaMineralna <raknieborak65@gmail.com>
2025-04-23 21:06:24 +0200

    ADD: optimistic updating to upvoting/downvoting

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,15 +1,22 @@
-import { use } from "react";
+import { use, useActionState, useOptimistic } from "react";
 import { OpinionsContext } from "../store/opinions-context";
-import { useActionState } from "react";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
 
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(
+    votes,
+    (prevVotes, customModeProp) =>
+      customModeProp === "up" ? prevVotes + 1 : prevVotes - 1
+  );
+
   async function upvoteAction() {
+    setVotesOptimistically("up");
     await upvoteOpinion(id);
   }
 
   async function downvoteAction() {
+    setVotesOptimistically("down");
     await downvoteOpinion(id);
   }
 
@@ -17,6 +24,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
     upvoteAction,
     null
   );
+
   const [downvoteFormState, downvoteFormAction, downvotePending] =
     useActionState(downvoteAction, null);
 
@@ -49,7 +57,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button
           formAction={downvoteFormAction}
